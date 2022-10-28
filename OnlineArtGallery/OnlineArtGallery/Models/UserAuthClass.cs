a6581736286472e366a7fd9fd0f57123272e9770
LaibaRazi <khanlayba72@gmail.com>
2022-10-28 20:49:54 +0500

    Authentication and Authorization

---
+++ OnlineArtGallery/OnlineArtGallery/Models/UserAuthClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineArtGallery.Models
+{
+    public class UserAuthClass
+    {
+        //id users fname lname email contacto cantactt adress region city country pcode gender pass
+
+        public int id { get; set; }
+        public string users { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string email { get; set; }
+        public string contacto { get; set; }
+        public string contactt { get; set; }
+        public string adress { get; set; }
+        public string region { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string pcode { get; set; }
+        public string gender { get; set; }
+        public string pass { get; set; }
+    }
+}
